5e015ded0832b07411384568a21bbe0f8511526c
tsubakimoto <tkmbmbr@gmail.com>
2015-10-02 01:20:28 +0900

    Change test successed

--- vsosample.test/UnitTest1.cs
+++ vsosample.test/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace vsosample.test
         public void TestMethod1()
         {
             var person = new vsosample.Person { Name = "Hoge" };
-            Assert.AreEqual("Bar", person.SayName());
+            Assert.AreEqual("Hoge", person.SayName());
         }
     }
 }
